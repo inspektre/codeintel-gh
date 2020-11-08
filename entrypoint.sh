f947071a4cd29a66b004951fb3699ed941d5d0cb
Uday Korlimarla <44824134+udaykor@users.noreply.github.com>
2020-11-08 18:54:50 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,34 @@
-ScanTarget=$GITHUB_WORKSPACE
-/tools/appinspector analyze -c high -s "$ScanTarget" -f json -o ApplicationInspector.json
-cat ApplicationInspector.json
+#!/bin/bash
+
+# $1 is the path relative to repo root to scan
+# $2 is the output path relative to repo root.
+# $3 is the output format
+# $4 is the comma separated list to pass to ignore
+# $5 is the arguments
+
+if [ ${3,,} = "html" ]; then
+    echo "html output is not supported in the GitHub Action."
+    exit -1
+fi
+
+if [ "$1" = "GITHUB_WORKSPACE" ]; then
+    ScanTarget=$GITHUB_WORKSPACE
+else
+    ScanTarget=$GITHUB_WORKSPACE/$1
+fi
+
+if [ "$2" = "AppInspectorResults" ]; then
+    OutputPath=$GITHUB_WORKSPACE/$2.$3
+else
+    OutputPath=$GITHUB_WORKSPACE/$2
+fi
+
+if [ "$4" != "," ]; then
+    IFS=',' read -ra SPLITS <<< "$4"
+    for i in "${SPLITS[@]}"; do
+        IgnoreArg = "${IgnoreArg},$GITHUB_WORKSPACE/$i"
+    done
+    IgnoreArg = "-k ${IgnoreArg:1}"
+fi
+
+/tools/appinspector analyze -s "$ScanTarget" -o "$OutputPath" -f $3 $IgnoreArg $5
