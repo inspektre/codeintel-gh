7e7e9c018738750e66a2f42d485c67f0ffc60185
Uday Korlimarla <44824134+udaykor@users.noreply.github.com>
2020-12-03 19:14:08 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,29 +6,29 @@
 # $4 is the comma separated list to pass to ignore
 # $5 is the arguments
 
-# if [ ${3,,} = "html" ]; then
-#     echo "html output is not supported in the GitHub Action."
-#     exit -1
-# fi
+if [ ${3,,} = "html" ]; then
+    echo "html output is not supported in the GitHub Action."
+    exit -1
+fi
 
-# if [ "$1" = "GITHUB_WORKSPACE" ]; then
-#     ScanTarget=$GITHUB_WORKSPACE
-# else
-#     ScanTarget=$GITHUB_WORKSPACE/$1
-# fi
+if [ "$1" = "GITHUB_WORKSPACE" ]; then
+    ScanTarget=$GITHUB_WORKSPACE
+else
+    ScanTarget=$GITHUB_WORKSPACE/$1
+fi
 
-# if [ "$2" = "AppInspectorResults" ]; then
-#     OutputPath=$GITHUB_WORKSPACE/$2.$3
-# else
-#     OutputPath=$GITHUB_WORKSPACE/$2
-# fi
+if [ "$2" = "AppInspectorResults" ]; then
+    OutputPath=$GITHUB_WORKSPACE/$2.$3
+else
+    OutputPath=$GITHUB_WORKSPACE/$2
+fi
 
-# if [ "$4" != "," ]; then
-#     IFS=',' read -ra SPLITS <<< "$4"
-#     for i in "${SPLITS[@]}"; do
-#         IgnoreArg = "${IgnoreArg},$GITHUB_WORKSPACE/$i"
-#     done
-#     IgnoreArg = "-k ${IgnoreArg:1}"
-# fi
-cd $GITHUB_WORKSPACE
-/tools/appinspector analyze -s . -o "$OutputPath" -f $3 $IgnoreArg $5
+if [ "$4" != "," ]; then
+    IFS=',' read -ra SPLITS <<< "$4"
+    for i in "${SPLITS[@]}"; do
+        IgnoreArg = "${IgnoreArg},$GITHUB_WORKSPACE/$i"
+    done
+    IgnoreArg = "-k ${IgnoreArg:1}"
+fi
+
+/tools/appinspector analyze -s $ScanTarget -o $OutputPath -f $3 $IgnoreArg $5
